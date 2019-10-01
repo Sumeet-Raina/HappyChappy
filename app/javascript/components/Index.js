c4f5238c2c816b3c308247b83287459beb12b5ef
kierepkae <kierepkaedyta@gmail.com>
2019-10-01 15:28:24 +0100

    add test for mood types on the page

--- app/javascript/components/Index.js
+++ app/javascript/components/Index.js
@@ -4,12 +4,12 @@ class Index extends React.Component {
   render() {
     return (
 
-      <div class='mood-container'>
-        <h1 class='welcome-sentence'>Hello. How are you feeling today?</h1>
-        <div class='mood-emoji'><img class='mood-emoji' src="https://cdn.pixabay.com/photo/2017/03/05/21/55/emoticon-2120024_960_720.png" alt="" srcset="" /></div>
-        <span class='mood-emoji'>😊</span>
-        <span class='mood-emoji'>😐</span>
-        <span class='mood-emoji'>😔</span>
+      <div className='mood-container'>
+        <h1 className='welcome-sentence'>Hello. How are you feeling today?</h1>
+        <h4 className="mood">good</h4>
+        <h4 className="mood">bad</h4>
+        <h4 className="mood">okay</h4>
+        <h4 className="mood">silly</h4>
       </div>
 
     );
