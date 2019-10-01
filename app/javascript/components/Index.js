5f036925c61b7ce678ae31c53ea3b56f44c05d3e
kierepkae <kierepkaedyta@gmail.com>
2019-10-01 12:53:34 +0100

    setup

---
+++ app/javascript/components/Index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Index extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Hello. How are you feeling today?</h1>
+      </>
+    );
+  }
+}
+
+export default Index
