fc86cdc65dd43f9ef0eaab86a6c860d134300b1b
kierepkae <kierepkaedyta@gmail.com>
2019-10-01 15:05:05 +0100

    try to setup emoji

--- app/javascript/components/Index.js
+++ app/javascript/components/Index.js
@@ -4,10 +4,13 @@ class Index extends React.Component {
   render() {
     return (
 
-      <>
-        <h1>Hello. How are you feeling today?</h1>
-        <p>😊</p>
-      </>
+      <div class='mood-container'>
+        <h1 class='welcome-sentence'>Hello. How are you feeling today?</h1>
+        <div class='mood-emoji'><img class='mood-emoji' src="https://cdn.pixabay.com/photo/2017/03/05/21/55/emoticon-2120024_960_720.png" alt="" srcset="" /></div>
+        <span class='mood-emoji'>😊</span>
+        <span class='mood-emoji'>😐</span>
+        <span class='mood-emoji'>😔</span>
+      </div>
 
     );
   }
