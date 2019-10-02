544469255ccf965b2c5b75ea425f6ea0f6329052
Kat <vampirebatkat@gmail.com>
2019-10-02 12:39:30 +0100

    feat: feature test added for sign up
    Co-authored-by  michaelatbreathehr <3543326+michaelatbreathehr@users.noreply.github.com>
    Co-authored by KMaskell <43371948+KMaskell@users.noreply.github.com>

--- spec/controllers/page_controller_spec.rb
+++ spec/controllers/page_controller_spec.rb
@@ -1,12 +1,19 @@
 # require 'rails_helper'
 
-# RSpec.describe PageController, type: :controller do
+RSpec.describe PageController, type: :controller do
 
-#   describe "GET #index" do
-#     it "returns http success" do
-#       get :index
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
+  # describe "GET #index" do
+  #   it "returns http success" do
+  #     get :index
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-# end
+  # describe "Successful login" do
+  #   it "returns emoticon page" do
+  #     get :index
+  #     expect(page).to have_content("How are you feeling today?")
+  #   end
+  # end
+
+end
