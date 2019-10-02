358cf9e80e2053225b12b8acafd5167c86ed21fa
kierepkae <kierepkaedyta@gmail.com>
2019-10-02 14:24:49 +0100

    try to fix problem with displaying mood count

--- app/javascript/components/Mood.js
+++ app/javascript/components/Mood.js
@@ -9,27 +9,43 @@ class Mood extends React.Component {
     okay: 0
   }
 
-  handleClick=(mood)=>{
-    
-    
-    this.setState({
-      [`${mood}`]: this.state[`${mood}`]+= 1
+  // handleClick = (mood) => {
+
+
+  //   this.setState((prevState) =>() {
+  //     [`${mood}`]: this.state[`${mood}`] += 1
+  //   })
+  //   console.log(this.state)
+  // }
+
+  handleClick(mood) {
+    this.setState((state) => {
+      return {
+        [`${mood}`]: this.state[`${mood}`] += 1
+
+      }
     })
+
     console.log(this.state)
+
   }
 
   render() {
     return (
-      <div onClick={()=> this.handleClick(this.props.moodType)} className="mood-container">
+      <div onClick={() => this.handleClick(this.props.moodType)} className="mood-container">
         <div className="mood-item">
           <h2 className="mood-type">{this.props.moodType}</h2>
           <img src={this.props.moodImage} alt="happy face" className='happy-img mood-img' />
         </div>
+        <p>{this.state.happy}</p>
+        <p>{this.state.sad}</p>
+        <p>{this.state.okay}</p>
+        <p>{this.state.silly}</p>
       </div>
-  
+
     );
   }
-} 
+}
 
 
 export default Mood
