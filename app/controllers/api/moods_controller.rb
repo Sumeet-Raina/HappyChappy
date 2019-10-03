bcde846bc490dce0d4185978da80e943ed527ed1
Stegosauruss <russell.wind@gmail.com>
2019-10-03 15:52:19 +0100

    fixed indentation

--- app/controllers/api/moods_controller.rb
+++ app/controllers/api/moods_controller.rb
@@ -1,13 +1,12 @@
 module API
   class MoodsController < ApplicationController
-
     def index
       moods = current_user.moods.all
-      happy_count = moods.count { |mood| mood.mood_type == "happy"}
-      sad_count = moods.count { |mood| mood.mood_type == "sad"}
-      okay_count = moods.count { |mood| mood.mood_type == "okay"}
-      silly_count = moods.count { |mood| mood.mood_type == "silly"}
-      render json: {happy: happy_count, sad: sad_count, okay: okay_count, silly: silly_count}
+      happy_count = moods.count { |mood| mood.mood_type == "happy" }
+      sad_count = moods.count { |mood| mood.mood_type == "sad" }
+      okay_count = moods.count { |mood| mood.mood_type == "okay" }
+      silly_count = moods.count { |mood| mood.mood_type == "silly" }
+      render json: { happy: happy_count, sad: sad_count, okay: okay_count, silly: silly_count }
     end
 
     def create
