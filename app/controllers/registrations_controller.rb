de46791d9fe7e5b5b78522dec9852ae88b086c16
Michael Redgrave <michael@breathehr.com>
2019-10-07 13:04:23 +0100

    add bio to users

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,6 +7,6 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:email, :user_name, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:email, :user_name, :bio, :password, :password_confirmation, :current_password)
   end
 end
