dc040c1e14951712908de937b431e9f98ed4e9fd
Stegosauruss <russell.wind@gmail.com>
2019-10-09 09:52:08 +0100

    Corrected formatting

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,6 @@ class MessagesController < ApplicationController
   def create
     message = Message.create(text: params[:text], conversation_id: params[:conversation_id], user_name: current_user.user_name)
     conversation = Conversation.find(params[:conversation_id])
-    MessagesChannel.broadcast_to conversation, {message: message, key: message.id}
+    MessagesChannel.broadcast_to conversation, { message: message, key: message.id }
   end
 end
