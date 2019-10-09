b1a0f892359839b0f23402fb23a971fe1ebb78eb
kierepkae <kierepkaedyta@gmail.com>
2019-10-09 15:04:56 +0100

    create RandomSillyAdvice

---
+++ app/javascript/components/RandomSillyAdvice.js
@@ -0,0 +1,12 @@
+import React from "react"
+import PropTypes from "prop-types"
+class RandomSillyAdvice extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+}
+
+export default RandomSillyAdvice
