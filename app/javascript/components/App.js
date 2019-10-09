33769fb11bca9b8e7749f71b1259a9eb4199f36b
kierepkae <kierepkaedyta@gmail.com>
2019-10-09 13:10:51 +0100

    extract piechart to MoodPieChart Component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -9,6 +9,7 @@ import { passCsrfToken } from '../util/helpers'
 import ChatBot from "react-simple-chatbot";
 import { ThemeProvider } from 'styled-components';
 import ChatRoom from './ChatRoom'
+import MoodPieChart from "./MoodPieChart";
 
 class App extends React.Component {
 
@@ -183,7 +184,7 @@ class App extends React.Component {
     }, {
       id: 'stats',
       component: (
-        <CustomComponent
+        <MoodPieChart
           data={this.state}
         />),
       trigger: 'chat'
@@ -217,80 +218,7 @@ class App extends React.Component {
   }
 }
 
-class CustomComponent extends React.Component {
 
-  state = {
-    happy: 0,
-    sad: 0,
-    silly: 0,
-    okay: 0,
-    currentMood: ""
-  }
-
-  componentDidMount() {
-    passCsrfToken(document, axios)
-    this.setMoods(this)
-  }
-
-  handleClick = (mood) => {
-    let currentMood = { currentMood: mood }
-    this.createMood(currentMood, this.setMoods)
-    this.setState({
-      currentMood: mood
-    })
-  }
-
-  createMood(currentMood, callback) {
-    axios
-      .post('/api/moods', currentMood)
-      .then(response => {
-        console.log(response)
-        console.log(response.data)
-        callback(this)
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }
-
-  setMoods(self) {
-    axios
-      .get('/api/moods')
-      .then(response => {
-        self.setState({
-          happy: response.data.happy,
-          sad: response.data.sad,
-          okay: response.data.okay,
-          silly: response.data.silly,
-          currentMood: response.data.currentMood
-        });
-      })
-  }
-
-  render() {
-    console.log(this.state)
-    return (
-      < PieChart
-        data={
-          [
-            { title: 'Okay', value: this.state.okay, color: '#C13C37' },
-            { title: 'Happy', value: this.state.happy, color: '#E38627' },
-            { title: 'Silly', value: this.state.silly, color: '#6A4335' },
-            { title: 'Sad', value: this.state.sad, color: '#6A2135' }
-          ]}
-        style={{ height: '15vw' }
-        }
-        label
-        animate
-        labelStyle={{
-          fontSize: '10px',
-          fontColor: '#FFFFFF',
-          fontFamily: 'sans-serif',
-          fill: '#121212'
-        }}
-      />)
-  }
-}
 
 export default App
 
