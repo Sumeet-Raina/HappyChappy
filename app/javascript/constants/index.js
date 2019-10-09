9b52a5d2cfcd30106aa7767fc57ffec1244c0090
Stegosauruss <russell.wind@gmail.com>
2019-10-09 14:17:04 +0100

    Added redis socket server

--- app/javascript/constants/index.js
+++
@@ -1,2 +0,0 @@
-export const API_ROOT = 'http://localhost:3000';
-export const API_WS_ROOT = 'ws://localhost:3000/cable';
--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -2,11 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../components/App'
 import { ActionCableProvider } from 'react-actioncable-provider'
-import { API_WS_ROOT } from '../constants'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <ActionCableProvider url={API_WS_ROOT}>
+    <ActionCableProvider>
       <App />
     </ActionCableProvider>,
     document.body.appendChild(document.createElement('div'))
