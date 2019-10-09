430c78450b7714fc907bb25cde35d4dd70df1111
kierepkae <kierepkaedyta@gmail.com>
2019-10-09 14:47:51 +0100

    update RandomSadAdvice Component

--- app/javascript/components/RandomSadAdvice.js
+++ app/javascript/components/RandomSadAdvice.js
@@ -4,13 +4,9 @@ class RandomSadAdvice extends React.Component {
 
   advice() {
     let advices = ["It’s okay to feel sad.", "Remember that it's temporary.",
-      "It’s normal to feel sad.", "What's the one way you can take care of yourself right now?", "It’s okay to feel sad. Be brave, reach out to someone, or write your thoughts down."
-    ]
+      "It’s normal to feel sad.", "What's the one way you can take care of yourself right now?", "It’s okay to feel sad. Be brave, reach out to someone, or write your thoughts down.", "You’re not alone in this.", "We are not on this earth to see through one another, but to see one another through.", "I can’t really understand what you are feeling, but I can offer my compassion.", "I’m sorry that you’re in so much pain. I am not going to leave you. I am going to take care of myself, so you don’t need to worry that your pain might hurt me.", "Have you tried chamomile tea?"]
 
     let index = Math.floor(Math.random() * advices.length)
-    console.log(advices)
-    console.log(index)
-    console.log(advices[index])
     return advices[index]
   }
   render() {
